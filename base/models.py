fd40369aeaaa8ca28fe8c8cf18871b2f047f935a
Ricardo Mazzolli <ricarm@gmail.com>
2019-03-27 14:26:34 -0300

    Issue #1 - Rotinas de registro e login

--- base/models.py
+++ base/models.py
@@ -1,76 +1,79 @@
+#coding: utf-8
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class Regional(models.Model):
-    nome = models.CharField(max_length=30)
+	nome = models.CharField(max_length = 30)
 
-    class Meta:
-        ordering = ('nome', )
-        verbose_name_plural = 'Regionais'
+	class Meta:
+		ordering = ('nome', )
+		verbose_name_plural = 'Regionais'
 
-    def __str__(self):
-        return u'%s' % self.nome
-
+	def __str__(self):
+		return self.nome
 
 class Categoria(models.Model):
-    nome = models.CharField(max_length=40)
-    descricao = models.TextField()
+	nome = models.CharField(max_length = 40)
+	descricao = models.TextField()
 
-    def __str__(self):
-        return u'%s' % self.nome
-
+	def __str__(self):
+		return self.nome
 
 class Premiacao(models.Model):
-    nome = models.CharField(max_length=40)
-    codigo = models.CharField(max_length=3)
-    ordem = models.SmallIntegerField()
+	nome = models.CharField(max_length = 40)
+	codigo = models.CharField(max_length = 3)
+	ordem = models.SmallIntegerField()
 
-    def __str__(self):
-        return u'%s' % self.nome
+	def __str__(self):
+		return self.nome
 
-    class Meta:
-        ordering = ('nome', )
-        verbose_name = 'Premiação'
-        verbose_name_plural = 'Premiações'
-
+	class Meta:
+		ordering = ('nome', )
+		verbose_name = 'Premiação'
+		verbose_name_plural = 'Premiações'
 
 class Formato(models.Model):
-    nome = models.CharField(max_length=40)
-    codigo = models.CharField(max_length=3)
-    premiacao = models.ForeignKey(Premiacao, null=True, on_delete=models.PROTECT)
-    cod_premiacao = models.CharField(max_length=3) # Temporário - só para o import
+	nome = models.CharField(max_length = 40)
+	codigo = models.CharField(max_length = 3)
+	premiacao = models.ForeignKey(Premiacao, null = True, on_delete = models.PROTECT)
+	cod_premiacao = models.CharField(max_length = 3) # Temporário - só para o import
 
-    def __str__(self):
-        return u'%s' % self.nome
-
+	def __str__(self):
+		return self.nome
 
 class Atividade(models.Model):
-    nome = models.CharField(max_length=40)
+	nome = models.CharField(max_length = 40)
 
-    def __str__(self):
-        return u'%s' % self.nome
+	def __str__(self):
+		return self.nome
 
-    class Meta:
-        ordering = ('nome', )
-
+	class Meta:
+		ordering = ('nome', )
 
 PREMIO_STATUS = (
-        ('A', u'Aberto para Inscrições'),
-        ('F', u'Inscrições encerradas'),
-        ('C', u'Concluído'),
-    )
-
+		('A', 'Aberto para Inscrições'),
+		('F', 'Inscrições encerradas'),
+		('C', 'Concluído'),
+	)
 
 class Premio(models.Model):
-    ano = models.SmallIntegerField()
-    premiacao = models.ForeignKey(Premiacao, on_delete=models.PROTECT)
-    regional = models.ForeignKey(Regional, on_delete=models.PROTECT)
-    status = models.CharField(max_length=1, choices=PREMIO_STATUS)
+	ano = models.SmallIntegerField()
+	premiacao = models.ForeignKey(Premiacao, on_delete = models.PROTECT)
+	regional = models.ForeignKey(Regional, on_delete = models.PROTECT)
+	status = models.CharField(max_length = 1, choices = PREMIO_STATUS)
 
-    def __str__(self):
-        return u'%s %s (%d)' % (self.premiacao,self.regional,self.ano)
+	def __str__(self):
+		return '{} {} ({})'.format(
+			self.premiacao,
+			self.regional,
+			self.ano
+		)
 
-    class Meta:
-        ordering = ('ano', )
-        verbose_name = u'Premio'
-        verbose_name_plural = u'Premios'
+	class Meta:
+		ordering = ('ano', )
+		verbose_name = 'Premio'
+		verbose_name_plural = 'Premios'
+
+class Usuario(User):
+	user = models.ForeignKey(User, null = True, blank = True, on_delete = models.CASCADE)
+	token = models.CharField(max_length = 32)
--- poweradmin/actions.py
+++ poweradmin/actions.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-import unicodecsv as csv
 from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.admin import helpers
