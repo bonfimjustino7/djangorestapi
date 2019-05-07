98999bf20c0e04f66981c3f1e7fd89868531aae6
josircg <josircg@gmail.com>
2019-05-07 14:35:28 -0300

    Merge Rafael

--- util/email.py
+++ util/email.py
@@ -9,57 +9,32 @@ from time import sleep
 from util.models import EmailAgendado
 
 
-def sendmail(subject='', to=[], params={}, template='', mimetype='text/html; charset=UTF-8', headers={}):
-    # from .models import Recurso
-    # ativo = Recurso.objects.get_or_create(recurso='EMAIL')[0].ativo
-    headers.update({'Reply-To': settings.REPLY_TO_EMAIL, })
-    '''
+def sendmail(subject='', to=[], params={}, template=''):
+    """
     Método para envio de e-mail:
     - subject: string contendo assunto do e-mail
     - to: lista dos e-mails dos destinatários
     - params: dicionário com os parâmetros para renderizar o e-mail
-    - template: string para o caminho do template do e-mail
-    - mimetype: string de tipo e charset do arquivo de e-mail, padrão 'text/html; charset=UTF-8'
-    '''
-    def send_thread_email(assunto='', to=[], params={}, template='', mimetype='', headers={}):
+    """
+    def send_thread_email(assunto='', to=[], params={}, template=''):
 
         email = EmailAgendado.objects.create(
             subject=assunto,
             to=','.join(to)
         )
 
-        text_content = assunto
         try:
             template_content = get_template(template)
             html_content = template_content.render(params)
             email.html = html_content
-            tentativas = 0
         except Exception as e:
             email.html = u'%s' % e
-            tentativa = 4
-        email.save()
 
-        while tentativas < 3:
-            try:
-                if settings.SEND_EMAIL:
-                    msg = EmailMultiAlternatives(assunto, text_content, settings.DEFAULT_FROM_EMAIL, bcc=to, headers=headers)
-                    msg.attach_alternative(email.html, mimetype)
-                    msg.send()
-                    email.status = 'K'
-                    email.save()
-                    break
-                else:
-                    email.status = 'A'
-                    email.save()
-                    break
-            except:
-                tentativas += 1
-                if tentativas < 3:
-                    email.status = 'R'
-                else:
-                    email.status = 'E'
-                email.save()
-                sleep(60)
+        if settings.SEND_EMAIL:
+            email.send()
+        else:
+            email.status = 'A'
+            email.save()
 
-    th = Thread(target=send_thread_email, args=(subject, to, params, template, mimetype, headers))
+    th = Thread(target=send_thread_email, args=(subject, to, params, template))
     th.start()
