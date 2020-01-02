416a9ac94909e0cee3e99883a41a794603421eab
josircg <josircg@gmail.com>
2020-01-02 09:03:57 -0300

    Erro timezone

--- menu.py
+++ menu.py
@@ -4,11 +4,10 @@ This file was generated with the custommenu management command, it contains
 the classes for the admin menu, you can customize this class as you want.
 
 To activate your custom menu add the following to your settings.py::
-    ADMIN_TOOLS_MENU = 'Cofivi.menu.CustomMenu'
+    ADMIN_TOOLS_MENU = '{{app}}.menu.CustomMenu'
 """
 
 from django.urls import reverse
-from django.utils.translation import ugettext_lazy as _
 from django.utils.text import capfirst
 from admin_tools.menu import items, Menu
 from admin_tools.menu.items import MenuItem
@@ -31,11 +30,11 @@ class CustomMenu(Menu):
     def __init__(self, **kwargs):
         Menu.__init__(self, **kwargs)
         self.children += [
-            items.MenuItem(_('Dashboard'), reverse('admin:index')),
+            items.MenuItem('Tela Inicial', reverse('admin:index')),
             items.Bookmarks(u'Favoritos'),
             CustomAppList(
-                u'CMS',
-                models=('cms.models.*', )
+                u'Inscrição',
+                models=('inscricao.models.*', )
             ),
             CustomAppList(
                 u'Adminstração',
