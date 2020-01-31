815998b73576051f27dbe457628922542ac43816
Bonfim Justino <bonfim1999@gmail.com>
2020-01-30 22:28:06 -0300

    Melhorando lógica de exibição das dicas

--- inscricao/static/inscricao/js/dicas.js
+++ inscricao/static/inscricao/js/dicas.js
@@ -2,38 +2,38 @@
 function getDica(id){
     $.get(`/dica_material/${id.trim()}/`, function (res) {
             if(!res.dica){
-              document.getElementById('dicas').style.display = 'none';
+              $('#dicas').hide();
+            }else {
+                $('#dicas').show();
+                document.getElementById('obs').innerText = res.dica;
+                console.log(res);
             }
-            document.getElementById('obs').innerText = res.dica;
-            console.log(res)
+
         })
 }
 function getDicaByName(name){
     $.get(`/dica_material/name/${name}/`, function (res) {
             if(!res.dica){
                 $('#dicas').hide();
+            }else{
+                $('#dicas').show();
+                 document.getElementById('obs').innerText = res.dica;
             }
-            document.getElementById('obs').innerText = res.dica;
-            console.log(res)
+            //console.log(res);
         })
 }
 $(document).ready(function () {
     $('<strong id="dicas">Dicas: <p id="obs"></p></strong>').appendTo('#tabs-2'); //criando o elemento
-    document.getElementById('dicas').style.display = 'none'; // ocultando o elemento
+    $('#dicas').hide(); // ocultando o elemento
 
     $('.dynamic-material_set select').change(function () {
-        document.getElementById('dicas').style.display = 'inline-block';
         getDica($(this).val())
-    })
+    });
     $('#id_videocase').focusin(function () {
-       $('#dicas').show();
        getDicaByName('Videocase');
-
-    })
+    });
     $('#id_apresentacao').focusin(function () {
-       $('#dicas').show();
        getDicaByName('Apresentação');
+    });
 
-    })
-
-})
+});
