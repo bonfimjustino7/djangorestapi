1f812971ca8f078c374079c8231caea5c9f28238
Bonfim Justino <bonfim1999@gmail.com>
2020-03-01 11:07:33 -0300

    Limpeza

--- inscricao/views.py
+++ inscricao/views.py
@@ -128,18 +128,6 @@ def finalizar(request):
                 messages.error(request, 'Você não tem nenhuma empresa inscrita.')
                 return redirect('/')
 
-def make_zipfile(output_filename, source_dir):
-    relroot = os.path.abspath(os.path.join(source_dir, os.pardir))
-    with zipfile.ZipFile(output_filename, "w", zipfile.ZIP_DEFLATED) as zip:
-        for root, dirs, files in os.walk(source_dir):
-            # add directory (needed for empty dirs)
-            zip.write(root, os.path.relpath(root, relroot))
-            for file in files:
-                filename = os.path.join(root, file)
-                if os.path.isfile(filename): # regular files only
-                    arcname = os.path.join(os.path.relpath(root, relroot), file)
-                    zip.write(filename, arcname)
-
 def empresa_download(request, id):
     empresa = Empresa.objects.get(id=id)
     path = settings.EXPORTACAO + '/%s_2020.csv' % empresa
